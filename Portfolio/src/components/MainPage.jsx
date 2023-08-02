da950ea7dcfabdff50e267651d35318a1509a263
unknown <ivan.piceno171@gmail.com>
2023-08-02 16:10:48 -0700

    basic card outline

--- Portfolio/src/components/MainPage.jsx
+++ Portfolio/src/components/MainPage.jsx
@@ -1,53 +1,63 @@
-import React from 'react'
-import '../sass/style.scss'
+import React from "react";
+import Projects from "./Projects";
 
 export default function MainPage() {
   return (
-    <div>
-    <nav>
-      <ol id="nav-ol">
-        <li className="nav-li">About</li>
-        <li className="nav-li">Projects</li>
-        <li className="nav-li">Skills</li>
-      </ol>
-    </nav>
+    <div className="main-page">
+      <nav>
+        <ol id="nav-ol">
+          <li className="nav-li">About</li>
+          <li className="nav-li">Projects</li>
+          <li className="nav-li">Skills</li>
+        </ol>
+      </nav>
 
-    <a className="big-logo" href="/index.html">
-      <img src="/img/ivan-logo-1 copy.png" alt="" id="logo" style={{ width: '15rem' }} />
-    </a>
+      <a className="big-logo" href="/index.html">
+        <img
+          src="/img/ivan-logo-1 copy.png"
+          alt=""
+          id="logo"
+          style={{ width: "15rem" }}
+        />
+      </a>
 
-    <div className="container-social-media">
-      <ul className="social-ul">
-        <li className="social-li"><a href=""><i className="fab fa-github"></i></a></li>
-        <li className="social-li"><a href=""><i className="fab fa-linkedin"></i></a></li>
-        <li className="social-li"><a href=""><i className="fas fa-envelope"></i></a></li>
-      </ul>
-    </div>
-
-    <div className="about-me-description">
-      <h1 className="nameh1">Ivan Piceno</h1>
-      <h2 className="goals-about">Full-Stack Developer</h2>
-    </div>
-    
-    {/* Skills */}
-    {/* <div className="skills-container">
-      <div className="skills-div">
-        <ul className="skills-ul">
-          <li className="skills-li">JavaScript</li>
-          <li className="skills-li">CSS</li>
-          <li className="skills-li">HTML</li>
-          <li className="skills-li">React</li>
+      {/* <div className="container-social-media animate__flipInY"> */}
+      <div className="container-social-media animate__backInRight">
+        <ul className="social-ul">
+          <li className="social-li">
+            <a href="">
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+          <li className="social-li">
+            <a href="">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li className="social-li">
+            <a href="">
+              <i className="fas fa-envelope"></i>
+            </a>
+          </li>
         </ul>
       </div>
-    </div> */}
 
-    <footer className="foot">
-      <div className="foot-description">created &amp; designed by Ivan Piceno</div>
-      <div className="foot-img">
-        <img id="footer-logo" src="/img/ivan-logo-1 copy.png" alt="" />
-        <img id="footer-logo" src="/img/ivan-logo-1 copy.png" alt="" />
+      <div className="about-me-description">
+        <h1 className="nameh1">Ivan Piceno</h1>
+        <h2 className="goals-about animate__hinge">Full-Stack Developer</h2>
       </div>
-    </footer>
-  </div>
-);
+
+      <Projects />
+
+      <footer className="foot">
+        <div className="foot-description">
+          created &amp; designed by Ivan Piceno
+        </div>
+        <div className="foot-img">
+          <img id="footer-logo" src="/img/ivan-logo-1 copy.png" alt="" />
+          <img id="footer-logo" src="/img/ivan-logo-1 copy.png" alt="" />
+        </div>
+      </footer>
+    </div>
+  );
 }
